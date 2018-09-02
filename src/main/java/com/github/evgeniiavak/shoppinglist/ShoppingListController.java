1e5772622ee3968b9d48d091a19368768ac022a5
EvgeniiaVak <27793901+EvgeniiaVak@users.noreply.github.com>
2018-09-02 20:20:52 +0700

    add naive item addition

--- src/main/java/com/github/evgeniiavak/shoppinglist/ShoppingListController.java
+++ src/main/java/com/github/evgeniiavak/shoppinglist/ShoppingListController.java
@@ -1,12 +1,12 @@
 package com.github.evgeniiavak.shoppinglist;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.BoundListOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @RestController
@@ -16,10 +16,39 @@ public class ShoppingListController {
     @Autowired
     private StringRedisTemplate redisTemplate;
 
+    private BoundListOperations<String, String> uncheckedList;
+    private BoundListOperations<String, String> checkedList;
+
+    @PostConstruct
+    public void init() {
+        uncheckedList = redisTemplate.boundListOps("shopping-list:unchecked");
+        checkedList = redisTemplate.boundListOps("shopping-list:checked");
+    }
+
+
     @GetMapping
     public ResponseEntity<ShoppingListModel> get() {
-        List<String> unchecked = redisTemplate.boundListOps("shopping-list:unchecked").range(0, -1);
-        List<String> checked = redisTemplate.boundListOps("shopping-list:checked").range(0, -1);
+        return getShoppingListEntity();
+    }
+
+    @PostMapping("checked")
+    public ResponseEntity<ShoppingListModel> postChecked(@RequestParam String item) {
+        checkedList.leftPush(item);
+        checkedList.trim(0, 30);
+
+        return getShoppingListEntity();
+    }
+
+    @PostMapping("unchecked")
+    public ResponseEntity<ShoppingListModel> postUnchecked(@RequestParam String item) {
+        uncheckedList.leftPush(item);
+
+        return getShoppingListEntity();
+    }
+
+    private ResponseEntity<ShoppingListModel> getShoppingListEntity() {
+        List<String> unchecked = uncheckedList.range(0, -1);
+        List<String> checked = checkedList.range(0, -1);
         return ResponseEntity.ok(new ShoppingListModel(unchecked, checked));
     }
 }
